41d8a9d6e7132f44ce647787034c0e36e50d94cf
Samuel Hendryx <samuelhendryx@outlook.com>
2024-03-02 15:57:07 -0800

    Define Media table.

---
+++ Models/Media.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Models
+{
+    public class Media
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int ReleaseYear { get; set; }
+        public string MediaType { get; set; } // Music, Video, etc
+        public TimeSpan Length { get; set; }
+        public string Container { get; set; }
+        public string Codec { get; set; }
+        public string? AudioFormat { get; set; }
+        public string? VideoFormat { get; set; }
+    }
+}

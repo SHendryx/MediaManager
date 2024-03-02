c53a8d7cc5868155626f9b99489bf40b055d7d2e
Samuel Hendryx <samuelhendryx@outlook.com>
2024-03-02 15:29:15 -0800

    Define file table. Closes #1

---
+++ Models/File.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MediaManager.Models
+{
+    public class File
+    {
+        public string FileName { get; set; }
+        public string FilePath { get; set; }
+        public string FileExtension { get; set; }
+        public string FileType { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreationTime { get; set; }
+    }
+}

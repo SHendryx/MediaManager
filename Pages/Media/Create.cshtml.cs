9ce671921e5ff2a2edb9a8fe276fe91242bad10e
Samuel Hendryx <samuelhendryx@outlook.com>
2024-03-03 07:22:21 -0800

    Add CRUD pages for MediaFile. Change MediaFile 'Length' to 'LengthSeconds'. Add File table.

---
+++ Pages/Media/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MediaManager.Data;
+using MediaManager.Models;
+
+namespace MediaManager.Pages.Media
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MediaManager.Data.MediaManagerContext _context;
+
+        public CreateModel(MediaManager.Data.MediaManagerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public MediaFile MediaFile { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.MediaFile.Add(MediaFile);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MediaManager.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<MediaManagerContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MediaManagerContext") ?? throw new InvalidOperationException("Connection string 'MediaManagerContext' not found.")));
 
 var app = builder.Build();
 

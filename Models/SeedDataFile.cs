aca0db12af85807127fbd79ee5948eba9bac0f5c
Samuel Hendryx <samuelhendryx@outlook.com>
2024-03-03 22:50:04 -0800

    Create SeedDataFile to seed file data.

---
+++ Models/SeedDataFile.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MediaManager.Data;
+
+namespace MediaManager.Models;
+
+public static class SeedData
+{
+    public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MediaManagerContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MediaManagerContext>>()))
+        {
+            if (context == null || context.File == null)
+            {
+                throw new ArgumentNullException("Null MediaManager");
+            }
+
+            // Look for any movies.
+            if (context.File.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+            context.File.AddRange(
+                new File
+                {
+                    FilePath = "",
+                    FileName = "",
+                    FileExtension = "",
+                    FileType = "",
+                    CreationTime = DateTime.Now,
+                }
+            );
+            context.SaveChanges();
+        }
+    }
+}
